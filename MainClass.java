018bd8bbf1d0f5ee655704f6469a05863cb37dc6
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-08-05 16:27:06 +0530

    Added : in main class file - simple msg

---
+++ MainClass.java
@@ -0,0 +1,8 @@
+package GamblingProg;
+
+public class MainClass {
+	public static void main(String[] args){
+		System.out.println("Assignment No. 7\n");
+		System.out.println("Gambling Simulation Problem");
+	}
+}
