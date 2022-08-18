1a71fad2c2e402f3c704d1e58ce55040d34a38f0
Mrunali Deore <107094589+MrunaliDeore@users.noreply.github.com>
2022-08-18 10:48:38 +0530

    As a Gambler, would start with a stake of $100 every day and bet $1 every game.

---
+++ Gambler.java
@@ -0,0 +1,12 @@
+package GamblingProg;
+
+public class Gambler {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome Gambling Stimulation Problem");
+		int playerCurrentBalance = 100;
+		int betAmountPerGame = 1;
+		System.out.println("Player Balance at start of day is "+playerCurrentBalance);
+		System.out.println("Betting amount per game is " +betAmountPerGame);
+	}
+}
